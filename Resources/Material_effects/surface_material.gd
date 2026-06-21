15c487e566705dcc6c9142450f9c4f34e8af7a4c
BlazeGTR <Blazejmajchrzak32@gmail.com>
2026-06-21 07:18:33 +0200

    asd

--- Resources/Material_effects/surface_material.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name SurfaceMaterial
-
-@export var name: String = "Concrete"
-@export var hit_effect: PackedScene
-@export var hit_sound: AudioStream
-@export var debris_color: Color = Color.WHITE
--- Scenes/Entities/Hostages/hostage.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-class_name Hostage
-
-@export_group("Score Settings")
-@export var score: int = 200
-@export var score_surrender_multiplier: float = 1.0 
-
-var patrol_wait_time: float = 0.0
